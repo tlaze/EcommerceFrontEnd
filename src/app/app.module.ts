2f186161bb563c793a634314454847601e25e91f
tlaze <tomlazore@gmail.com>
2023-05-08 14:23:38 -0400

    basic file structure complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserControllerComponent } from './Controller/user-controller/user-controller.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserControllerComponent,
+    NavbarComponent,
+    RegistrationComponent,
+    LoginComponent,
+    HomepageComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
