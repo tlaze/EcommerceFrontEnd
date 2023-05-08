65c47c8be191af374b7ac8123bad3cbdecbad455
tlaze <tomlazore@gmail.com>
2023-05-08 14:36:02 -0400

    routes addes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from 'src/app/pages/registration/registration.component';
+import { LoginComponent } from 'src/app/pages/login/login.component';
+import { HomepageComponent } from 'src/app/pages/homepage/homepage.component';
+import { CheckoutComponent } from 'src/app/pages/checkout/checkout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "registration", component: RegistrationComponent},
+  {path: "login", component: LoginComponent},
+  {path: "home", component: HomepageComponent},
+  {path: "checkout", component: CheckoutComponent},
+  {path: "**", component: HomepageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserControllerComponent } from './Controller/user-controller/user-controller.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -13,7 +12,6 @@ import { CheckoutComponent } from './pages/checkout/checkout.component';
 @NgModule({
   declarations: [
     AppComponent,
-    UserControllerComponent,
     NavbarComponent,
     RegistrationComponent,
     LoginComponent,
