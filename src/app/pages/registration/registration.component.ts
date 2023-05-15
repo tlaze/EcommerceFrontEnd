66aebf39fb3fe3d460ecba8dfcab677f48b5d45b
tlaze <tomlazore@gmail.com>
2023-05-15 11:38:01 -0400

    fixed bug where users could't register if they had an error message also styling to buttons

--- src/app/pages/registration/registration.component.ts
+++ src/app/pages/registration/registration.component.ts
@@ -16,34 +16,30 @@ export class RegistrationComponent {
 
   invalidCredentials:boolean = false;
   duplicateAccount:boolean = false;
-  public tabIndex = 1;
+  accountCreated:boolean = false;
 
   constructor(private router: Router, private authService: AuthService){}
-
-    ngOnInit():void {
-      this.authService.getRegisteredUsers().subscribe();
-    }
 
     idAsNumber(id : number | undefined) : number {
       return id as number;
     }
 
-
   registerSubmit():void{
 
     let newUser: Account = {username:this.username, password:this.password, balance:0, isLoggedIn:false}
     this.authService.getRegisteredUsers().subscribe(data => {
-//       console.log(data);
+      this.duplicateAccount = false;
       data.forEach((users) => {
         if(this.username == users.username){
-//           console.log("duplicate");
           this.duplicateAccount = true;
+          this.accountCreated = false;
         }
       })
+
       if(!this.duplicateAccount){
+        this.duplicateAccount = false;
+        this.accountCreated = true;
         this.authService.registerSubmit(newUser).subscribe();
-        const tabCount = 2;
-        this.tabIndex = (this.tabIndex + 1) % tabCount;
       }
     })
   }
@@ -52,27 +48,22 @@ export class RegistrationComponent {
 
     this.authService.getRegisteredUsers().subscribe(users => {
       for(let i = 0; i < users.length; i++){
-//         console.log(users[i]);
         if(this.username == users[i].username && this.password == users[i].password){
           this.authService.loginSubmit(this.idAsNumber(users[i].id)).subscribe(data => {
-//             console.log(data);
           })
+          this.invalidCredentials = false;
           localStorage.setItem("login", this.idAsNumber(users[i].id) as unknown as string);
           localStorage.setItem("id", this.idAsNumber(users[i].id).toString());
           this.authService.isLoggedIn = true;
           this.authService.loginID = this.idAsNumber(users[i].id);
           this.authService.loginSubmit(this.idAsNumber(users[i].id)).subscribe(data => {
-//             console.log(data);
             this.router.navigateByUrl('home');
           })
         }
         else{
-          console.log("doesn't exist");
           this.invalidCredentials = true;
         }
-
       }
     })
   }
-
 }
